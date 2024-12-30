6c775bcc475c460869e967f897055a0931725d19
farzaneh <jfarzan88@gmail.com>
2024-12-29 18:44:30 -0800

    delete cute folder

--- cute/cutecrashcourse/build/Desktop-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_cutecrashcourse_FILE /home/Kazazi/farzantest/cute/cutecrashcourse/build/Desktop-Debug/cutecrashcourse)
-set(__QT_DEPLOY_TARGET_cutecrashcourse_TYPE EXECUTABLE)
