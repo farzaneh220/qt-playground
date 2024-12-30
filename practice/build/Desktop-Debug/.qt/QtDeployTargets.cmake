6a30785be521cc683494ba88ad2b9afed42bb61a
farzaneh <jfarzan88@gmail.com>
2024-12-29 18:41:43 -0800

    project cleanup

--- practice/build/Desktop-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_practice_FILE /home/Kazazi/farzantest/practice/build/Desktop-Debug/practice)
-set(__QT_DEPLOY_TARGET_practice_TYPE EXECUTABLE)
--- project1/build/Desktop-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_project1_FILE /home/Kazazi/farzantest/project1/build/Desktop-Debug/project1)
-set(__QT_DEPLOY_TARGET_project1_TYPE EXECUTABLE)
